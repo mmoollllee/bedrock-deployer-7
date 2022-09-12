3700df551688846aa773e3197ff1ee9091e8f501
MGraf <info@moritz-graf.de>
2022-09-12 11:12:40 +0200

    Deployer 7

--- examples/deploy.full-example.php
+++ examples/deploy.full-example.php
@@ -13,23 +13,19 @@ require 'vendor/mmoollllee/bedrock-deployer/recipe/sage.php';
 require 'vendor/mmoollllee/bedrock-deployer/recipe/trellis.php';
 
 // Configuration
-
-set('bin/composer', function () { return 'composer'; });
+// set('bin/composer', function () { return 'composer'; });
+set('bin/composer', function () { return '/opt/plesk/php/7.4/bin/php /usr/lib/plesk-9.0/composer.phar'; });
 
 // Common Deployer config
 set( 'repository', 'git@github.com:example/example.git' );
-set( 'shared_dirs', [
-	'web/app/uploads'
-] );
+set( 'shared_dirs', ['web/app/uploads'] );
 
 // Bedrock DB config
 set( 'vagrant_dir', dirname( __FILE__ ) . '/../trellis' );
 set( 'vagrant_root', '/srv/www/example.com/current' );
 
-// Bedrock DB config
-set( 'local_root', dirname( __FILE__ ) );
-
-// Sage config
+// Bedrock DB and Sage config
+set( 'local_root', dirname( __FILE__ ) );;
 set( 'theme_path', 'web/app/themes/template' );
 
 // File transfer config
@@ -37,27 +33,25 @@ set( 'sync_dirs', [
 	dirname( __FILE__ ) . '/web/app/uploads/' => '{{deploy_path}}/shared/web/app/uploads/',
 ] );
 
-
 // Hosts
 
-set( 'default_stage', 'staging' );
-
-host( 'stage.example.com' )
-	->stage( 'staging' )
-	->user( 'user' )
+host( 'dev.example.com' )
+	->set('labels', ['stage' => 'dev'])
+	->set('remote_user', 'user')
 	->set( 'deploy_path', '~/stage/deploy' );
 // Set Webspace-Path to ~/stage/deploy/current/web/
 
-host( 'stillhammerhaus.de' )
-	->stage( 'production' )
-	->user( 'user' )
+host( 'example.com' )
+	->set('labels', ['stage' => 'live'])
+	->set('remote_user', 'user')
 	->set( 'deploy_path', '~/httpdocs/deploy' );
 
 
 // Tasks
 
 // Deployment flow
-desc( 'Deploy your project' );
+// ToDo adapt sage:vendors...
+desc( 'Deploy whole project' );
 task( 'deploy', [
 	'bedrock:prepare',
 	'deploy:lock',
@@ -69,13 +63,35 @@ task( 'deploy', [
 	'deploy:symlink',
 	'bedrock:env',
 	'bedrock:vendors',
-	'bedrock:wp-scss',
 	'deploy:clear_paths',
 	'push:db',
-	'push:files',
+	'push:files-no-bak',
 	'deploy:unlock',
-	'cleanup',
-	'success',
+	'deploy:cleanup',
+	'deploy:success',
+] );
+
+desc( 'Deploy only app' );
+task( 'push', [
+	'bedrock:prepare',
+	'deploy:lock',
+	'deploy:release',
+	'deploy:update_code',
+	'trellis:remove',
+	'deploy:shared',
+	'deploy:writable',
+	'bedrock:env',
+	'bedrock:vendors',
+	'deploy:clear_paths',
+	'deploy:symlink',
+	'deploy:unlock',
+	'deploy:cleanup',
+	'deploy:success',
+] );
+
+task( 'pull', [
+	'pull:db',
+	'pull:files',
 ] );
 
 // [Optional] if deploy fails automatically unlock.
