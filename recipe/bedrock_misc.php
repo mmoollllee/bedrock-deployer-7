668482857ac49298b6f5b661da7d7422e3a5587a
MGraf <info@moritz-graf.de>
2022-09-12 14:55:56 +0200

    Dotenv Fix?

--- recipe/bedrock_misc.php
+++ recipe/bedrock_misc.php
@@ -13,8 +13,3 @@ desc( 'Installing Bedrock vendors' );
 task( 'bedrock:vendors', function () {
     run( 'cd {{release_path}} && {{bin/composer}} {{composer_options}}' );
 } );
-
-desc( 'Create WP-SCSS Cache Folder' );
-task( 'bedrock:wp-scss', function () {
-    run( 'mkdir {{release_path}}/web/app/plugins/wp-scss/cache');
-} );
